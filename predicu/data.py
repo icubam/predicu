4c4b8c3c841af4126d3f7db36c54ac5e21d71f8a
Achillethin <achille.thin@free.fr>
2020-03-31 18:17:09 +0200

    add pre icubam

--- predicu/data.py
+++ predicu/data.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def load_and_preprocess_data(bedcount_path):
+def load_and_preprocess_data(bedcount_path,pre_icubam_path):
   ext = bedcount_path.rsplit('.', 1)[-1]
   if ext == 'pickle':
     with open(bedcount_path, 'rb') as f:
@@ -15,6 +15,32 @@ def load_and_preprocess_data(bedcount_path):
     d = pd.read_csv(bedcount_path)
   else:
     raise ValueError(f'unknown extension {ext}')
+  prior = pd.read_csv(pre_icubam_path)
+  prior_rn = prior.rename(columns={'Hopital': 'icu_name', 'NbSortieVivant':'n_covid_healed', 'NbCOVID': 'n_covid_occ', 
+              'NbLitDispo':'n_covid_free', 'NbDeces': 'n_covid_deaths', 'Date':'date'})
+  prior_rn.loc[prior_rn['icu_name']=='C-Scweitzer','icu_name'] = 'C-Schweitzer'
+  prior_rn.loc[prior_rn['icu_name']=='Bezannes','icu_name'] = 'C-Bezannes'
+
+
+  prior_rn['date']= pd.DatetimeIndex(pd.to_datetime(prior_rn['date'], unit='s')).normalize()
+  pre = prior_rn.groupby(['date', 'icu_name'])[['n_covid_occ','n_covid_free','n_covid_deaths','n_covid_healed']].last().reset_index()
+  pre.fillna(0)
+
+  for i,d in enumerate(pre['date'].unique()):
+    hosp = pre.loc[pre['date']==d, 'icu_name'].unique()
+    if i==0:
+        hosp_last = hosp
+        d_last = d
+    else:
+        for h in hosp_last:
+            if h not in hosp:
+                row = pre[pre['date']==d_last]
+                row = row[row['icu_name']==h]
+                row['date'] = d
+                pre.append(row)
+        hosp_last=h
+        d_last=d
+
   # columns with cumulative values from which we will compute the per day diff
   cum_columns = [
     'n_covid_free', 'n_covid_occ', 'n_covid_deaths', 'n_covid_healed'
@@ -53,4 +79,6 @@ def load_and_preprocess_data(bedcount_path):
                   break
       clean_data_points.append(new_data_point)
       per_icu_prev_data_point[icu_name] = new_data_point
+  curr = pd.DataFrame(clean_data_points)
+  total = pd.concat([pre, curr])
   return pd.DataFrame(clean_data_points)
