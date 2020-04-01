6ddc165b962d5637053467b943265c1fd41d050c
Valentin Iovene <valentin@too.gy>
2020-04-01 09:24:47 +0200

    sum values from same icus

--- check_data/icu_names_old_vs_new.py
+++ check_data/icu_names_old_vs_new.py
@@ -1,19 +1,33 @@
-from predicu.data import load_icubam_bedcount_data, load_pre_icubam_data
+from predicu.data import (
+  load_all_data, load_icubam_bedcount_data, load_pre_icubam_data
+)
 
 pre_icubam_path = 'data/pre_icubam_data.csv'
-old = load_pre_icubam_data(pre_icubam_path)
+pre_icubam = load_pre_icubam_data(pre_icubam_path)
 icubam_bedcount_path = 'data/bedcount_2020-03-31.pickle'
-new = load_icubam_bedcount_data(icubam_bedcount_path)
+icubam = load_icubam_bedcount_data(icubam_bedcount_path)
+combined = load_all_data(icubam_bedcount_path, pre_icubam_path)
 
-old_icu_names = set(old.icu_name.unique())
-new_icu_names = set(new.icu_name.unique())
+pre_icubam_icu_names = set(pre_icubam.icu_name.unique())
+icubam_icu_names = set(icubam.icu_name.unique())
+combined_icu_names = set(combined.icu_name.unique())
 
-print('icu_name in old data but not in new data')
+print('icu_name in pre-icubam but not in icubam')
 print('-' * 30)
-for icu_name in (old_icu_names - new_icu_names):
+for icu_name in (pre_icubam_icu_names - icubam_icu_names):
   print(icu_name)
 print('-' * 30)
-print('icu_name in old data but not in new data')
+print('icu_name in icubam but not in pre-icubam')
 print('-' * 30)
-for icu_name in (new_icu_names - old_icu_names):
+for icu_name in (icubam_icu_names - pre_icubam_icu_names):
+  print(icu_name)
+print('-' * 30)
+print('icu_name in pre-icubam but not in combined')
+print('-' * 30)
+for icu_name in (pre_icubam_icu_names - combined_icu_names):
+  print(icu_name)
+print('-' * 30)
+print('icu_name in icubam but not in combined')
+print('-' * 30)
+for icu_name in (icubam_icu_names - combined_icu_names):
   print(icu_name)
