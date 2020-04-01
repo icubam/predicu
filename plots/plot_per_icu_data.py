1763226d53619f969f854de35da5ad03367be985
Valentin Iovene <valentin@too.gy>
2020-04-01 12:32:17 +0200

    fix data join and max increase fix
    when diff between last good value's date and current date is
    greater than 1 day, adjust the max increase to handle that
    difference

--- plots/plot_per_icu_data.py
+++ plots/plot_per_icu_data.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.style
 import seaborn as sns
 
-from predicu.data import load_all_data
+from predicu import load_all_data
+from predicu.data import CUM_COLUMNS
 
 matplotlib.style.use('seaborn-whitegrid')
 
@@ -13,13 +14,14 @@ data = load_all_data(
   icubam_bedcount_path=icubam_bedcount_path, pre_icubam_path=pre_icubam_path
 )
 
-columns = ['n_covid_free', 'n_covid_occ', 'n_covid_deaths', 'n_covid_healed']
+n_rows = (len(CUM_COLUMNS) + len(CUM_COLUMNS) % 2) // 2
 
-fig = plt.figure(figsize=(20, 20))
-gs = matplotlib.gridspec.GridSpec(2, 2)
-for i, col in enumerate(columns):
+fig = plt.figure(figsize=(n_rows * 7, 14))
+gs = matplotlib.gridspec.GridSpec(n_rows, 2)
+for i, col in enumerate(CUM_COLUMNS):
   ax = fig.add_subplot(gs[i // 2, (i + 1) % 2])
   sns.lineplot(x='date', y=col, data=data)
   ax.set_title(col)
 fig.suptitle('Grand Est daily increases averaged over ICUs')
+fig.tight_layout()
 plt.show()
