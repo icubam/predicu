cf31872a668c24b6ca5c1aac782d543e48c557b4
Valentin Iovene <valentin@too.gy>
2020-04-03 22:48:59 +0200

    plotting

--- plots/plot_barplot_per_day.py
+++ plots/plot_barplot_per_day.py
@@ -3,8 +3,8 @@ import datetime
 import matplotlib.gridspec
 import matplotlib.pyplot as plt
 import matplotlib.style
-import pandas as pd
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import tikzplotlib
 
@@ -13,35 +13,46 @@ from predicu import load_all_data, load_pre_icubam_data
 from predicu.data import CUM_COLUMNS
 from predicu.plot import COLUMN_COLOR, COLUMN_TO_HUMAN_READABLE
 
-matplotlib.style.use('seaborn-darkgrid')
+matplotlib.style.use("seaborn-darkgrid")
 
 icus = set(
-  list(
-    load_pre_icubam_data(predicu.data.DEFAULT_PRE_ICUBAM_PATH
-                         ).icu_name.unique()
-  )
+    list(
+        load_pre_icubam_data(
+            predicu.data.DEFAULT_PRE_ICUBAM_PATH
+        ).icu_name.unique()
+    )
 )
 
-print('nb of icus', len(icus))
+print("nb of icus", len(icus))
 print(sorted(list(icus)))
 
-column = 'n_covid_deaths'
+column = "n_covid_deaths"
 d = load_all_data()
-__import__('pdb').set_trace()
 d = d.loc[d.icu_name.isin(icus)]
-d = d[['date', 'icu_name', 'department', column, 'datetime']]
-d = d.groupby(['date', 'icu_name']).max().reset_index()
-d = d.groupby(['date', 'department']).sum().reset_index()
 d = d.loc[d.date < pd.to_datetime("2020-04-03")]
+d = d.groupby(["date", "department"]).sum().reset_index()
+d = d.sort_values(by="date")
 fig, ax = plt.subplots(1, figsize=(20, 10))
-sns.barplot(x='date', y=column, data=d, ax=ax)
-ax.axvline(6.5, c='red', alpha=0.7, ls='dotted', lw=2.0)
-ax.set_xticklabels([date.strftime('%d/%m') for date in d.date.unique()],
-                   rotation=45,
-                   fontdict={'fontsize': 'x-small'})
-ax.set_ylabel('Décès par jour et réanimation')
+
+x = d.date.values
+y = (
+    d.n_covid_deaths.values
+    / d.department.apply(predicu.data.load_department_population().get).values
+    * 1e5
+)
+
+sns.barplot(x=x, y=y, ax=ax, ci='sd', capsize=.2)
+ax.axvline(6.5, c="red", alpha=0.7, ls="dotted", lw=2.0)
+ax.set_xticklabels(
+    [date.strftime("%d/%m") for date in d.date.unique()],
+    rotation=45,
+    fontdict={"fontsize": "x-small"},
+)
+ax.set_ylabel("Décès (cumulés) par département")
 ax.set_xlabel(None)
-txt = ax.text(6.5, 3.5, 'Début acquisition ICUBAM', fontsize='large')
-txt.set_horizontalalignment('center')
+txt = ax.text(6.5, 32, "Début acquisition ICUBAM", fontsize="large")
+txt.set_horizontalalignment("center")
 plt.show()
-# tikzplotlib.save('reports/build/figs/barplot_deaths_per_day.tex', standalone=True)
+# tikzplotlib.save(
+    # "reports/build/figs/barplot_deaths_per_day.tex", standalone=True
+# )
--- plots/plot_per_icu_data.py
+++ plots/plot_per_icu_data.py
@@ -1,23 +1,88 @@
+import matplotlib.cm
 import matplotlib.gridspec
+import matplotlib.patches
 import matplotlib.pyplot as plt
 import matplotlib.style
+import numpy as np
 import seaborn as sns
+import tikzplotlib
 
-from predicu import load_all_data
-from predicu.data import CUM_COLUMNS
+from predicu import load_all_data, load_pre_icubam_data
+from predicu.data import CUM_COLUMNS, load_icu_name_to_department
 
-matplotlib.style.use('seaborn-whitegrid')
+matplotlib.style.use("seaborn-dark")
+
+pre_icubam_data = load_pre_icubam_data()
+grand_est_icu_names = set(pre_icubam_data.icu_name.unique())
 
 data = load_all_data()
+data = data.loc[data.icu_name.isin(grand_est_icu_names)]
+# last known total number of deaths per icu
+data = data.groupby("icu_name").n_covid_deaths.max()
+# remove icus with zero deaths
+data = data.loc[data > 0]
+# sort values by nb of deaths (descending)
+data = data.sort_values()
+# reset index
+data = data.reset_index()
 
-fig = plt.figure(figsize=(10, len(CUM_COLUMNS) * 10))
-gs = matplotlib.gridspec.GridSpec(len(CUM_COLUMNS), 1)
-ax0 = None
-for i, col in enumerate(CUM_COLUMNS):
-  ax = fig.add_subplot(gs[i, 0], sharex=ax0)
-  if ax0 is None: ax0 = ax
-  sns.lineplot(x='date', y=col, data=data)
-  ax.set_title(col)
-fig.suptitle('Grand Est daily increases averaged over ICUs')
-fig.tight_layout()
-fig.savefig('fig.pdf')
+icu2dpt = load_icu_name_to_department()
+data["department"] = data["icu_name"].apply(icu2dpt.get)
+data.groupby('department').n_covid_deaths.sum().reset_index()
+
+unique_dpts = sorted(list(set(list(data["department"].unique()))))
+colors = matplotlib.cm.Set2(np.linspace(1, 0, len(unique_dpts)))
+dpt2color = dict(zip(unique_dpts, colors))
+
+fig, ax = plt.subplots(1, figsize=(3, 8))
+
+for i, row in data.iterrows():
+    rect_patch = matplotlib.patches.Rectangle(
+        xy=(0, i),
+        width=row["n_covid_deaths"],
+        height=1,
+        fill=True,
+        linewidth=0.7,
+        edgecolor="white",
+        facecolor=dpt2color[row["department"]],
+        label=row["department"],
+    )
+    ax.add_patch(rect_patch)
+    ax.text(
+        row["n_covid_deaths"],
+        i + 0.15,
+        r"\Large{{{}}}".format(row["n_covid_deaths"]),
+    )
+ax.set_xlim(0, data["n_covid_deaths"].iloc[-1] + 3)
+ax.set_ylim(0, len(data))
+xticks = [5, 10, 15, 20]
+for xtick in xticks:
+    ax.axvline(x=xtick, ls="dashed", c="w")
+ax.set_xticks(xticks)
+ax.set_yticks(np.arange(len(data)) + 0.5)
+ax.set_yticklabels(data.icu_name)
+ax.legend(
+    handles=[
+        matplotlib.patches.Patch(facecolor=dpt2color[dpt], label=dpt)
+        for dpt in unique_dpts
+    ],
+    loc="lower right",
+)
+# ax.set_title(r'Nbr de décès par réa (18 mars $\rightarrow$ 3 avril)')
+# fig.savefig('fig.pdf')
+extra_axis_parameters = {
+    r"xticklabel style={font=\scriptsize}",
+    r"yticklabel style={font=\scriptsize}",
+}
+extra_tikzpicture_parameters = {
+    # r"every axis legend/.code={\let\addlegendentry\relax}"
+}
+tikzplotlib.save(
+    "/tmp/fig.tex",
+    standalone=True,
+    axis_width="8cm",
+    axis_height="12cm",
+    textsize=8.0,
+    extra_axis_parameters=extra_axis_parameters,
+    extra_tikzpicture_parameters=extra_tikzpicture_parameters,
+)
