c411bd82fa094d3983f5fa34e4a8860bc5232821
Valentin Iovene <valentin@too.gy>
2020-04-07 22:22:43 +0200

    big refactoring for deployable plot generation

---
+++ predicu/plots/lineplot_cumsum_per_dept.py
@@ -0,0 +1,40 @@
+import matplotlib.gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+
+import predicu.data
+import predicu.plot
+
+
+def plot(**plot_args):
+    data = predicu.data.load_all_data(api_key=plot_args["api_key"])
+    n_rows = (
+        len(predicu.data.CUM_COLUMNS) + len(predicu.data.CUM_COLUMNS) % 2
+    ) // 2
+    fig = plt.figure(figsize=(n_rows * 5, 10))
+    gs = matplotlib.gridspec.GridSpec(n_rows, 2)
+    ax0 = None
+    for i, col in enumerate(predicu.data.CUM_COLUMNS):
+        ax = fig.add_subplot(gs[i // 2, i % 2], sharex=ax0)
+        if ax0 is None:
+            ax0 = ax
+        for g, d in data.groupby("department"):
+            d = d.groupby("date")[col].sum().sort_index().cumsum()
+            ax.plot(np.arange(d.values.shape[0]), d.values, label=g)
+        ax.set_title(predicu.plot.COLUMN_TO_HUMAN_READABLE[col])
+        dates = sorted(list(data.date.unique()))
+        ax.set_xticks(np.arange(d.values.shape[0]))
+        ax.set_xticklabels(
+            [date.strftime("%d/%m") for date in dates],
+            rotation=45,
+            fontdict={"fontsize": "x-small"},
+        )
+    ax0.legend(ncol=2, loc="upper left", frameon=True, fontsize="xx-small")
+    fig.subplots_adjust(hspace=0.7)
+    tikzplotlib_kwargs = dict(
+        extra_groupstyle_parameters={
+            r"horizontal sep=0.2cm",
+            r"vertical sep=3cm",
+        }
+    )
+    return fig, tikzplotlib_kwargs
