148b218ad9b65583c74f4e069c097a2efad7bc3f
Roman Yurchak <rth.yurchak@gmail.com>
2020-04-08 17:58:57 +0200

    Move generate_plots to predicu.plots

--- predicu/tests/test_plot.py
+++ predicu/tests/test_plot.py
@@ -2,8 +2,8 @@ import pytest
 import os
 from pathlib import Path
 
-from predicu.plots.__main__ import generate_plots
 import predicu.plots
+from predicu.plots import generate_plots
 import predicu.data
 
 
@@ -14,16 +14,17 @@ def test_generate_plots_wrong_name():
         generate_plots(plots=["invalid2"])
 
 
-@pytest.mark.parametrize('name', predicu.plots.PLOTS)
+@pytest.mark.parametrize("name", predicu.plots.PLOTS)
 def test_generate_plots(name, tmpdir, monkeypatch):
     output_dir = str(tmpdir.mkdir("sub"))
     monkeypatch.setitem(
         predicu.data.DATA_PATHS,
         "icubam",
-        os.path.join(predicu.data.BASE_PATH,
-        "tests/data/fake_all_bedcounts_2020-04-08_16h41.csv"),
+        os.path.join(
+            predicu.data.BASE_PATH,
+            "tests/data/fake_all_bedcounts_2020-04-08_16h41.csv",
+        ),
     )
     generate_plots(plots=[name], output_dir=output_dir)
 
-    assert (Path(output_dir) / (name + '.png')).exists()
-
+    assert (Path(output_dir) / (name + ".png")).exists()
