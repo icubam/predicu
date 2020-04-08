b2a66e75ae199bb888a8ec6d683dbd55a00184ca
Roman Yurchak <rth.yurchak@gmail.com>
2020-04-08 16:52:34 +0200

    Improve predicu/plots/__main__.py

--- predicu/tests/test_plot.py
+++ predicu/tests/test_plot.py
@@ -1,3 +1,9 @@
-def test_empty():
+from predicu.plots.__main__ import generate_plots
+
+
+def test_generate_plots():
     """Empty test to make pytest pass"""
-    ...
+    args = {}
+    generate_plots(**args)
+    
+
