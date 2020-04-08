114e5840ab4c944122bbdba7a60321d648569538
Roman Yurchak <rth.yurchak@gmail.com>
2020-04-08 15:50:31 +0200

    Fix compile_all_plots.sh

--- compile_all_plots.sh
+++ compile_all_plots.sh
@@ -2,8 +2,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-rm /tmp/predicu_cache.h5
-for file in `ls plots/*.py`; do
+rm -f /tmp/predicu_cache.h5
+for file in `ls predicu/plots/*.py`; do
     if python "$file"; then
         echo "${GREEN}compiled plot $file${NC}"
     else
