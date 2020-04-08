12cc6362818b31fd5b21416ad298927fba315976
Roman Yurchak <rth.yurchak@gmail.com>
2020-04-08 17:09:21 +0200

    Add test for making plots

--- predicu/tests/test_plot.py
+++ predicu/tests/test_plot.py
@@ -1,9 +1,25 @@
+import pytest
+import os
+
 from predicu.plots.__main__ import generate_plots
+import predicu.plots
+import predicu.data
 
 
-def test_generate_plots():
-    """Empty test to make pytest pass"""
-    args = {}
-    generate_plots(**args)
-    
+def test_generate_plots_wrong_name():
 
+    msg = "Unknown plot.* invalid2"
+    with pytest.raises(ValueError, match=msg):
+        generate_plots(plots=["invalid2"])
+
+
+@pytest.mark.parametrize('name', predicu.plots.PLOTS)
+def test_generate_plots(name, tmpdir, monkeypatch):
+    output_dir = str(tmpdir.mkdir("sub"))
+    monkeypatch.setitem(
+        predicu.data.DATA_PATHS,
+        "icubam",
+        os.path.join(predicu.data.BASE_PATH,
+        "tests/data/fake_all_bedcounts_2020-04-08_16h41.csv"),
+    )
+    generate_plots(plots=[name], output_dir=output_dir)
